5b507def4d8ad67439a350829bd18a3bd3682560
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-31 15:49:56 -0700

    Public path update for github pages

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/link-ent/',
     proxyTable: {},
 
     // Various Dev Server settings
