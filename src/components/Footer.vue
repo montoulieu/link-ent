7c940f98234d4ba12bea7675480d3325c41e2350
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-31 15:45:46 -0700

    Removing text from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,6 @@
 <template>
   <footer class="py-5">
-    <p>LinkEnt is a lightweight <a href="https://linktr.ee/">Linktree</a> clone, built with Bootstrap 4 and Vue.js for Github Pages.</p>
-    <p>Consider the <a href="">source</a> and clone from the "new-leaf" branch to start with a clean slate.</p>
-    <p>If you find this useful feel free to buy me a coffee <i class="fa fa-coffee"></i></p>
-    <DonateButton/>
+    <p class="mb-0">Built with <a href="">LinkEnt</a></p>
   </footer>
 </template>
 
