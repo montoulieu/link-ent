5c338582765d8666101d89f4bbd71d2b9e83724b
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-31 15:41:48 -0700

    Reverting publicPath to slash

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/link-ent/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
