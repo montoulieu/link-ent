d6e4588da67c8ec11208217ecc31a4599fda8403
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-31 16:20:10 -0700

    Fixed incorrect asset path change

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/link-ent/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/link-ent/',
 
     /**
      * Source Maps
