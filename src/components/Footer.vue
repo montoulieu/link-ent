f5ff0dd9c96ee2424db5f46f6678d985fb753702
Pieter Montoulieu <pi.mont@icloud.com>
2019-01-31 18:47:40 -0700

    Unnecessary comma

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="py-4">
-    <p>LinkEnt is a <a href="https://linktr.ee/" target="_blank">Linktree</a> alternative, built with Bootstrap 4 and Vue.js for Github Pages.</p>
+    <p>LinkEnt is a <a href="https://linktr.ee/" target="_blank">Linktree</a> alternative built with Bootstrap 4 and Vue.js for Github Pages.</p>
     <p>Consider the <a href="https://github.com/pi-mont/link-ent" target="_blank">source</a> and clone from the "new-leaf" branch to start fresh.</p>
     <p class="mb-4">If you find this useful,<br class="d-block d-md-none"> feel free to buy me a coffee <i class="fa fa-coffee"></i></p>
     <DonateButton/>
