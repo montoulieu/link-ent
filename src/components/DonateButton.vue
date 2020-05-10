f87b99663bdfeeb8234b85c9dfff9e093a73a28d
Pieter Montoulieu <pieter@montoulieu.dev>
2020-05-10 16:13:03 -0600

    Deleting all files to prepare for React/Next setup

--- src/components/DonateButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" class="mb-2">
-    <input type="hidden" name="cmd" value="_s-xclick" />
-    <input type="hidden" name="hosted_button_id" value="VWL33SJH88RNW" />
-    <button type="submit" class="btn btn-link btn-sm text-uppercase font-weight-bold mb-2">Buy Me A Coffee</button>
-    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-  </form>
-</template>
