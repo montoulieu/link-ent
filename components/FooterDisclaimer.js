08cec945db3127562acbd58d07a457ee87c7eb37
Pieter Montoulieu <pieter@montoulieu.dev>
2020-05-11 00:08:03 -0600

    Adding donation button

--- components/FooterDisclaimer.js
+++ components/FooterDisclaimer.js
@@ -1,12 +1,15 @@
-function Footer() {
+import DonateButton from "./DonateButton";
+
+function FooterDisclaimer() {
   return (
     <div className="text-xs">
-      <p className="pb-2">LinkEnt is a free Linktree alternative built with React and Tailwind. <br className="hidden sm:block"/>
+      <p className="mb-2">LinkEnt is a free Linktree alternative built with React and Tailwind. <br className="hidden sm:block"/>
       Consider the source and clone from the <span className="text-green-400">"new-leaf"</span> branch to create your own. <br className="hidden sm:block"/>
       If you find this useful, feel free to buy me a coffee <i className="fa fa-coffee"></i></p>
-      <p>Built by <a href="https://github.com/montoulieu" target="_blank" className="text-green-400">Pieter Montoulieu</a></p>
+      <p className="mb-2">Built by <a href="https://github.com/montoulieu" target="_blank" className="text-green-400">Pieter Montoulieu</a></p>
+      <DonateButton/>
     </div>
   );
 }
 
-export default Footer;
+export default FooterDisclaimer;
