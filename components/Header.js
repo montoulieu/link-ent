33fa49710b56bf723799c4a49e67f3d80db3839d
Pieter Montoulieu <pieter@montoulieu.dev>
2020-05-11 00:00:03 -0600

    Link and style updates

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 
 function Header() {
   return (
-    <header className="container flex text-center mx-auto py-8">
+    <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/profile-pic.jpg" className="h-24 rounded-full mx-auto mb-5" />
         <h1 className="font-bold">@montoulieu</h1>
