c46e93e0153419016b646c0a54b52ef9b03d0874
Pieter Montoulieu <pieter@montoulieu.dev>
2020-05-16 15:17:55 -0600

    Style edits and fix for class in LinkList

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,8 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/profile-pic.jpg" className="h-24 rounded-full mx-auto mb-5" />
-        <h1 className="font-bold">@montoulieu</h1>
+        <h1 className="font-bold">Pieter Montoulieu</h1>
+        <p>Full Stack Engineer, Game Developer and Audio Engineer</p>
       </div>
     </header>
   );
