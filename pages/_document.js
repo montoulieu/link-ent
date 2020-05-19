f170293e5d13b32f6f1f218cb13d050dd361d30e
Pieter Montoulieu <pieter@montoulieu.dev>
2020-05-19 11:27:31 -0600

    Accessibility fixes for links, profile image and html language

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+export default CustomDocument
