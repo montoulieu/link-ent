f8aa7fd8f1fbc0396447779a535e1dd8e61cb4e1
Pieter Montoulieu <pieter@montoulieu.dev>
2020-05-19 11:42:05 -0600

    Adding version to footer

--- components/FooterDisclaimer.js
+++ components/FooterDisclaimer.js
@@ -1,4 +1,5 @@
 import DonateButton from "./DonateButton";
+import packageJson from '../package.json';
 
 function FooterDisclaimer() {
   return (
@@ -6,7 +7,8 @@ function FooterDisclaimer() {
       <p className="">Built by <a href="https://github.com/montoulieu" target="_blank" className="text-green-400" rel="noopener">Pieter Montoulieu</a></p>
       <p className="mb-3"><a href="https://github.com/montoulieu/link-ent" target="_blank" className="text-green-400" rel="noopener">LinkEnt</a> is a free Linktree alternative built with React and Tailwind. <br className="hidden sm:block"/>
       If you find this useful, feel free to buy me a coffee <i className="fa fa-coffee"></i></p>
-      <DonateButton/>
+      <DonateButton />
+      v{packageJson.version}
     </div>
   );
 }
